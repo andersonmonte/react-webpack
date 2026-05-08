8e659f1953eb2f2e13c563017ba24447f8f55989
Anderson Monte <andersonxef@gmail.com>
2026-05-07 22:01:56 -0300

    Hot loader feature configured

--- src/app.js
+++ src/app.js
@@ -2,11 +2,11 @@
 
 import React from 'react'
 
-var Title = React.createClass({
+const App = React.createClass({
     render: function () {
-        return <h1>Título</h1>
+        return <h1>Aplicação</h1>
     }
 })
 
-// module.exports = Title
-export default Title
+// module.exports = App
+export default App
